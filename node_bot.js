7b883bd8639651b31e5cfbabe933506676d30ec0
lexnovic <56923204+lexnovic@users.noreply.github.com>
2022-08-14 20:30:01 +0300

    Update node_bot.js

--- node_bot.js
+++ node_bot.js
@@ -18,17 +18,20 @@ getArticle()
   });
 
 async function getArticle() {
+  // get object from wiki
     let article = await axios.get('https://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts&exchars=500&format=json')
     console.log (article);
     return article;
 };
 
 function sendWikiarticle (articleUrl){
+  // send wiki url to the chat
     bot.api.sendMessage('-1001616695538', 'Веталь зайка');
     bot.api.sendMessage('-1001616695538', articleUrl);
 };
 
 async function scheduleWiki(){
+  //get page id from article and add it to the url
 schedule.scheduleJob('11 * * * * *', async function(){
     let article = await getArticle();
     let pageid = await getArticle().data.query.pages;
