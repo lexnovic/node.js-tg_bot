aa73285846eb72cb8370aa834ef694eec3e03935
lexnovic <56923204+lexnovic@users.noreply.github.com>
2022-08-20 18:36:56 +0300

    Update node_bot.js

--- node_bot.js
+++ node_bot.js
@@ -17,16 +17,8 @@ async function getArticle() {
     return article;
 };
 
-function sendWikiarticle (articleUrl){
-  // send wiki url to the chat
-  //  bot.api.sendMessage('-1001616695538', 'Веталь зайка');
-  //  bot.api.sendMessage('-1001616695538', articleUrl);
-  app.telegram.sendMessage('-1001616695538', articleUrl);
-};
-
 schedule.scheduleJob('11 * * * * *', async function(){
    let article = await getArticle();
-  //  let pageid = await getArticle().data.query.pages;
     var pageid = Object.keys(article.data.query.pages)[0]
     console.log(pageid);
     let articleURL = `https://en.wikipedia.org/wiki?curid=${pageid}`;
@@ -34,38 +26,15 @@ schedule.scheduleJob('11 * * * * *', async function(){
     sendWikiarticle (articleURL);
 });
 
-
-// axios
-//   .get('https://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts&exchars=500&format=json')
 getArticle()
   .then(res => {
     console.log(`statusCode: ${res.status}`);
-    //console.log(res);
   })
   .catch(error => {
     console.error(error);
   });
 
-// getPage()
-//   .then(res => {
-//     console.log(`statusCode: ${res.status}`);
-//     //console.log(res);
-//   })
-//   .catch(error => {
-//     console.error(error);
-//   });
-
-function sendWikiarticle (articleUrl){
-  // send wiki url to the chat
-  //  bot.api.sendMessage('-1001616695538', 'Веталь зайка');
-  //  bot.api.sendMessage('-1001616695538', articleUrl);
-  app.telegram.sendMessage('-1001616695538', articleUrl);
-};
-
-// scheduleWiki()
-// .then(res => {
-//     console.log(res);
-//   })
-// .catch(error => {
-//     console.error(error);
-//   });
+  function sendWikiarticle (articleUrl){
+    // send wiki url to the chat
+    app.telegram.sendMessage('-1001616695538', articleUrl);
+  };
